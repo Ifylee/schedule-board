c0e224fa69a11e21a98eb53ef8981576996a86ca
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-06-05 00:45:07 -0500

    added codes to my js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,47 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
+const generateTaskId = function() {
 
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
+const createTaskCard = function(task) {
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+const renderTaskList = function() {
+    if(!taskList) {
+        taskList = [];
+    }
 
+    const todoList = $("#todo-cards");
+    todoList.empty();
+
+    const inProgressList = $("#in-progress-cards");
+    inProgressList.empty();
+
+    const doneList = $("#done-cards");
+    doneList.empty();
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+const handleAddTask = function(event){
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+ const handleDeleteTask = function(event){
 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
+const handleDrop = function(event, ui) {
 
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList();
 });
